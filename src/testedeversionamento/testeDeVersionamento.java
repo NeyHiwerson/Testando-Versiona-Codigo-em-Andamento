711cc0af6a493d165792e69dac615c4cb0a85825
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2021-12-22 16:32:58 -0300

    editadoMain
    editei o main mas ta complicado importar

--- src/testedeversionamento/testeDeVersionamento.java
+++ src/testedeversionamento/testeDeVersionamento.java
@@ -5,6 +5,7 @@ public class testeDeVersionamento {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esse é outro versionamento do eclipse");
+		System.out.println("uma HORA VAI NA AOUTRA NÃO");
 
 	}
 
